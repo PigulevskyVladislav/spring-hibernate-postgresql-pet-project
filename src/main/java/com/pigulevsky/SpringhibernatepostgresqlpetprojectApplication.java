8a312a0d52d6e414b09d25642e941340facde314
v.pigulevsky <vlad.pigulevsky@gmail.com>
2023-03-28 14:23:12 +0300

    000 - first commit

---
+++ src/main/java/com/pigulevsky/SpringhibernatepostgresqlpetprojectApplication.java
@@ -0,0 +1,13 @@
+package com.pigulevsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringhibernatepostgresqlpetprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringhibernatepostgresqlpetprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pigulevsky/SpringhibernatepostgresqlpetprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pigulevsky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringhibernatepostgresqlpetprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
